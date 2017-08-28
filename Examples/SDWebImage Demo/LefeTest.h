0dfe8c2b42ca85ee191539058f6afaee91318bc7
wangsuyan <wsyxyxs@126.com>
2017-08-28 14:29:18 +0800

    添加文件

---
+++ Examples/SDWebImage Demo/LefeTest.h
@@ -0,0 +1,13 @@
+//
+//  LefeTest.h
+//  SDWebImage Demo
+//
+//  Created by WangSuyan on 2017/8/28.
+//  Copyright © 2017年 Dailymotion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LefeTest : NSObject
+
+@end
---
+++ Examples/SDWebImage Demo/LefeTest.m
@@ -0,0 +1,13 @@
+//
+//  LefeTest.m
+//  SDWebImage Demo
+//
+//  Created by WangSuyan on 2017/8/28.
+//  Copyright © 2017年 Dailymotion. All rights reserved.
+//
+
+#import "LefeTest.h"
+
+@implementation LefeTest
+
+@end
